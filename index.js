120f75000d959aba7ae59dcf82cd2fbee55497b1
Sean R Reid <sean@torchcodelab.com>
2020-09-14 16:09:15 -0400

    Added external data via db.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const express = require("express");
 const app = express();
 
 const server = http.createServer(app);
+const db = require('./db');
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`);
@@ -40,6 +41,33 @@ const helloController = (req, res) => {
         .end();
 }
 
+const monsterController = (req, res) => {
+    let snippet = `<h1>BOOO!!!</h1>`;
+
+    if (req.params.name === undefined && db !== undefined) {
+        snippet += `<ul>`;
+        db.map((monster) => {
+            snippet += `<li><a href="/monster/${monster.slug}">${monster.name}</a></li>`;
+        });
+        snippet += `</ul>`;
+    }
+
+    if (req.params.name !== undefined) {
+        snippet = `<h1>BOOO!!!!</h1>`;
+
+        db.map(monster => {
+            if (req.params.name === monster.slug) {
+                snippet += `<p>I am ${monster.name}, and I am a ${monster.species}.</p>`;
+            }
+        });
+    }
+    res
+        .status(200)
+        .send(snippet)
+        .end();
+}
+
 app.get("/", rootController); // <- ROOT route
 app.get("/foo", fooController); // <- FOO route
 app.get("/hello/:name?", helloController); // <- HELLO route
+app.get("/monster/:name?", monsterController);
